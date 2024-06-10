2987736f70ff00ab86170b6299bf7937bb2cf75f
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2024-06-10 09:59:22 +0300

    Use fetch one to get exact results

--- webapp/flowengine/apps/frontend/modules/signon/actions/actions.class.php
+++ webapp/flowengine/apps/frontend/modules/signon/actions/actions.class.php
@@ -123,7 +123,7 @@ class signonActions extends sfActions
                 $profile->setRegisteras(6);
                 $profile->save();
             } else {
-                $user_share = Doctrine_Core::getTable('sfGuardUserProfile')->findByUserId($this->sfGuardUser->id);
+                $user_share = Doctrine_Core::getTable('sfGuardUserProfile')->findByUserId($this->sfGuardUser->id)->fetchOne();
                 error_log("Profile exists yes or no");
                 error_log($user_share);
                 if ($user_share) {

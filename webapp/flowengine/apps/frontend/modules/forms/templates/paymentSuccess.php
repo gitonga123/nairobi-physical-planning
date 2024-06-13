188ba6759d6e3ccd6da93af7609e83321f37c645
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2024-06-13 11:45:05 +0300

    Show successful payment when the user is redirected to this page

--- webapp/flowengine/apps/frontend/modules/forms/templates/paymentSuccess.php
+++ webapp/flowengine/apps/frontend/modules/forms/templates/paymentSuccess.php
@@ -1,3 +1,14 @@
+<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+	<symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+		<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+	</symbol>
+	<symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+		<path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+	</symbol>
+	<symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+		<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+	</symbol>
+</svg>
 <div class="col-md-7 col-lg-8 col-xl-9">
 	<div class="col-12">
 		<nav class="user-tabs">
@@ -7,41 +18,82 @@
 				</li>
 			</ul>
 		</nav>
-		<div class="accordion mb-3" id="accordionCheckoutForm">
-			<div class="accordion-item">
-				<h2 class="accordion-header" id="headingTwo">
-					<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-						Checkout
-					</button>
-				</h2>
-				<div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-					<div class="accordion-body">
-						<form class="form-horizontal" id="checkout_initial_payment" action="<?php echo '/index.php/forms/initiatePayment/application/' . $application->getId() . '/invoice/' . $invoice->getId(); ?>">
-							<div id="response_area_id"></div>
-							<div class="form-group" style="margin: 2px;">
-								<label for="phone_number">Phone Number:</label>
-								<input type="text" class="form-control input-lg" id="phone_number" placeholder="Phone Number" name="phone_number" value="<?php echo $user->getProfile()->getMobile(); ?>">
-							</div>
-							<div class="form-group p-t-10" style="margin: 2px; margin-top:10px;">
-								<button type="submit" class="btn btn-md btn-dark" id="initiate_payment_loader">
-									<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true" style="display: none;"></span>
-									Initiate Payment
-								</button>
-							</div>
+		<?php if ($invoice->getPaid() == 1) : ?>
+			<div class="accordion mb-3" id="accordionCheckoutForm">
+				<div class="accordion-item">
+					<h2 class="accordion-header" id="headingTwo">
+						<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+							Checkout
+						</button>
+					</h2>
+					<div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+						<div class="accordion-body">
+							<form class="form-horizontal" id="checkout_initial_payment" action="<?php echo '/index.php/forms/initiatePayment/application/' . $application->getId() . '/invoice/' . $invoice->getId(); ?>">
+								<div id="response_area_id"></div>
+								<div class="form-group" style="margin: 2px;">
+									<label for="phone_number">Phone Number:</label>
+									<input type="text" class="form-control input-lg" id="phone_number" placeholder="Phone Number" name="phone_number" value="<?php echo $user->getProfile()->getMobile(); ?>">
+								</div>
+								<div class="form-group p-t-10" style="margin: 2px; margin-top:10px;">
+									<button type="submit" class="btn btn-md btn-dark" id="initiate_payment_loader">
+										<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true" style="display: none;"></span>
+										Initiate Payment
+									</button>
+								</div>
 
-						</form>
-						<div class="form_container" style="display: none;" id="form_checkout_container">
-							<div id="checkout_form_options_id"></div>
+							</form>
+							<div class="form_container" style="display: none;" id="form_checkout_container">
+								<div id="checkout_form_options_id"></div>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		<?php elseif ($invoice->getPaid() == 2) : ?>
+
+			<div class="alert alert-success mt-2" role="alert">
+				<div class="d-flex align-items-center">
+					<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:">
+						<use xlink:href="#check-circle-fill" />
+					</svg>
+					Invoice Already Paid.
+				</div>
+			</div>
+			<div class="mb-3">
+
+				<div class="d-flex justify-content-between">
+					<a class="btn btn-outline-info btn-block me-5" href="/index.php/application/view/id/<?php echo $application->getId(); ?>">Application Details</a>
+
+					<a class="btn btn-outline-success btn-block" href="/index.php/invoices/view/id/<?php echo $invoice->getId(); ?>">Invoice Details</a>
+
+				</div>
+
+			</div>
+		<?php else : ?>
+			<div class="alert alert-success mt-2" role="alert">
+				<div class="d-flex align-items-center">
+					<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:">
+						<use xlink:href="#check-circle-fill" />
+					</svg>
+					Invoice Invalid (Expired/Cancelled)
+				</div>
+			</div>
+			<div class="mb-3">
+
+				<div class="d-flex justify-content-between">
+					<a class="btn btn-outline-info btn-block me-5" href="/index.php/application/view/id/<?php echo $application->getId(); ?>">Application Details</a>
+
+					<a class="btn btn-outline-success btn-block" href="/index.php/invoices/view/id/<?php echo $invoice->getId(); ?>">Invoice Details</a>
+
+				</div>
+
+			</div>
+		<?php endif; ?>
 		<div class="accordion mb-3" id="accordionInvoice Info">
 			<div class="accordion-item">
 				<h2 class="accordion-header" id="headingOne">
 					<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-						Invoice # <?php echo $invoice->getInvoiceNumber(); ?>
+						Invoice # <?php echo $invoice->getInvoiceNumber(); ?> - Fee(s)
 					</button>
 				</h2>
 

75f3a7aa1a95089d3d5d16e86ab9feac3119247d
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2024-07-26 14:19:01 +0300

    Fix backend logo as well

--- webapp/flowengine/apps/backend/modules/dashboard/templates/_settingsheader.php
+++ webapp/flowengine/apps/backend/modules/dashboard/templates/_settingsheader.php
@@ -8,91 +8,90 @@
  * @subpackage dashboard
  * @author     Webmasters Africa / Thomas Juma (thomas.juma@webmastersafrica.com)
  */
- use_helper('I18N');
+use_helper('I18N');
 ?>
 <div class="leftpanel">
   <div class="logopanel">
-  <?php
-	// $agency_manager = new AgencyManager();
-	// $logo = $agency_manager->getLogo($sf_user->getAttribute('userid'));
-	?>
-      <!-- <h1> <img src="<?php echo strlen($logo)? $logo:'/assets_backend/images/logo.png' ?>" alt="" /></h1> -->
-      <h1> <img src="/asset_mentor/admin/assets/img/logo2.png" alt="" /></h1>
+    <?php
+    // $agency_manager = new AgencyManager();
+    // $logo = $agency_manager->getLogo($sf_user->getAttribute('userid'));
+    ?><img src="/asset_mentor/admin/assets/img/logo2.png" alt="" class="img img-fluid img--fullwidth"/>
   </div><!-- logopanel -->
   <?php
   //Displays the sidemenu
   include_component('dashboard', 'settingssidemenu');
   ?>
 
-<div class="mainpanel">
+  <div class="mainpanel">
 
-  <div class="headerbar">
+    <div class="headerbar">
 
-    <a class="menutoggle"><i class="fa fa-bars"></i></a>
+      <a class="menutoggle"><i class="fa fa-bars"></i></a>
 
-    <form class="searchform" action="/backend.php/applications/search" method="post">
-      <input type="text" class="form-control" name="applicationid" placeholder="<?php echo __('Search'); ?>" />
-    </form>
+      <form class="searchform" action="/backend.php/applications/search" method="post">
+        <input type="text" class="form-control" name="applicationid" placeholder="<?php echo __('Search'); ?>" />
+      </form>
 
-    <form class="searchform" action="/backend.php/applications/search" method="post">
-      <a href="/backend.php/applications/search?search=adv" class="btn btn-default pull-left btn-advanced m-t-8"><span class="fa fa-search"></span>  <?php echo __('Advanced Search'); ?></a>
-    </form>
-    <div class="header-right">
-      <ul class="headermenu">
-      <?php
-        if(sizeof($languages) > 1)
-        {
-      ?>
-        <li>
-          <div class="btn-group">
-            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-              <span class="fa fa-language"></span>
-            </button>
-            <ul class="dropdown-menu dropdown-menu-usermenu pull-right">
-              <?php
-              foreach($languages as $locale)
-              {
-                  $selected = "";
-                  if($locale->getLocaleIdentifier() == $sf_user->getCulture())
-                  {
-                    $selected = "active";
+      <form class="searchform" action="/backend.php/applications/search" method="post">
+        <a href="/backend.php/applications/search?search=adv" class="btn btn-default pull-left btn-advanced m-t-8"><span
+            class="fa fa-search"></span> <?php echo __('Advanced Search'); ?></a>
+      </form>
+      <div class="header-right">
+        <ul class="headermenu">
+          <?php
+          if (sizeof($languages) > 1) {
+            ?>
+            <li>
+              <div class="btn-group">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                  <span class="fa fa-language"></span>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-usermenu pull-right">
+                  <?php
+                  foreach ($languages as $locale) {
+                    $selected = "";
+                    if ($locale->getLocaleIdentifier() == $sf_user->getCulture()) {
+                      $selected = "active";
+                    }
+                    ?>
+                    <li class="<?php echo $selected; ?>"><a
+                        href="/backend.php/languages/setlocale/code/<?php echo $locale->getLocaleIdentifier(); ?>"><i
+                          class="glyphicon glyphicon-cog"></i> <?php echo $locale->getLocalTitle(); ?></a></li>
+                    <?php
                   }
                   ?>
-                  <li class="<?php echo $selected; ?>"><a href="/backend.php/languages/setlocale/code/<?php echo $locale->getLocaleIdentifier(); ?>"><i class="glyphicon glyphicon-cog"></i> <?php echo $locale->getLocalTitle(); ?></a></li>
+                </ul>
+              </div>
+            </li>
+            <?php
+          }
+          ?>
+          <li>
+            <div class="btn-group">
+              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                <?php
+                if ($logged_reviewer->getProfilePic()) {
+                  ?>
+                  <img
+                    src="<?php echo $site_settings->getUploadDirWeb(); ?><?php echo $logged_reviewer->getProfilePic(); ?>"
+                    alt="" />
                   <?php
-              }
-              ?>
-            </ul>
-          </div>
-        </li>
-        <?php
-        }
-        ?>
-        <li>
-          <div class="btn-group">
-            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-              <?php
-              if($logged_reviewer->getProfilePic())
-              {
+                } else {
+                  ?>
+                  <img src="/assets_backend/images/photos/loggeduser.png" alt="" />
+                  <?php
+                }
                 ?>
-                <img src="<?php echo $site_settings->getUploadDirWeb(); ?><?php echo $logged_reviewer->getProfilePic(); ?>" alt="" />
-                <?php
-              }
-              else
-              {
-                ?>
-                <img src="/assets_backend/images/photos/loggeduser.png" alt="" />
-                <?php
-              }
-              ?>
-            </button>
-            <ul class="dropdown-menu dropdown-menu-usermenu pull-right">
-              <li><a href="/backend.php/dashboard/profile"><i class="glyphicon glyphicon-cog"></i> <?php echo $logged_reviewer->getStrfirstname()." ".$logged_reviewer->getStrlastname(); ?></a></li>
-              <li><a href="/backend.php/login/logout"><i class="glyphicon glyphicon-log-out"></i> <?php echo __('Log Out'); ?></a></li>
-            </ul>
-          </div>
-        </li>
-      </ul>
-    </div><!-- header-right -->
+              </button>
+              <ul class="dropdown-menu dropdown-menu-usermenu pull-right">
+                <li><a href="/backend.php/dashboard/profile"><i class="glyphicon glyphicon-cog"></i>
+                    <?php echo $logged_reviewer->getStrfirstname() . " " . $logged_reviewer->getStrlastname(); ?></a></li>
+                <li><a href="/backend.php/login/logout"><i class="glyphicon glyphicon-log-out"></i>
+                    <?php echo __('Log Out'); ?></a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div><!-- header-right -->
 
-  </div><!-- headerbar -->
+    </div><!-- headerbar -->

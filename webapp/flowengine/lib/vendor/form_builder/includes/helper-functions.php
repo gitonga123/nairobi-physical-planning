d5aa175152c2efa20f10c5bc34ce336744108d05
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2024-08-20 15:27:57 +0300

    remove console messages

--- webapp/flowengine/lib/vendor/form_builder/includes/helper-functions.php
+++ webapp/flowengine/lib/vendor/form_builder/includes/helper-functions.php
@@ -2110,9 +2110,6 @@ function mf_get_logic_javascript_block_plot_verification($dbh, $form_id, $page_n
 			}
 
 			function fillSelectElement(valueName, selectElementId) {
-				
-				console.log(valueName);
-				console.log(selectElementId);
 				valueName = valueName.toLowerCase();
 				$('#' + selectElementId + ' option').prop('selected', false);
 				$('#'+selectElementId + ' option').each(function() {

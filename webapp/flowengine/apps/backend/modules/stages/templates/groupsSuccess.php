6d7520fa838faa232be892ad809047933d96facc
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2025-02-12 09:37:34 +0300

    Update a little bit of permissions

--- webapp/flowengine/apps/backend/modules/stages/templates/groupsSuccess.php
+++ webapp/flowengine/apps/backend/modules/stages/templates/groupsSuccess.php
@@ -1,84 +1,96 @@
 <?php
-  use_helper("I18N");
+use_helper("I18N");
 ?>
 <div class="contentpanel">
 
-    <form id="stageform" class="form-bordered" action="/plan/stages/groups/id/<?php echo $stage->getId(); ?>" method="post" autocomplete="off" data-ajax="false">
-      <div class="panel panel-default">
+  <form id="stageform" class="form-bordered" action="/plan/stages/groups/id/<?php echo $stage->getId(); ?>"
+    method="post" autocomplete="off" data-ajax="false">
+    <div class="panel panel-default">
 
       <div class="panel-heading">
-        <h3 class="panel-title"><?php echo $stage->getTitle().' Stage'; ?></h3>
+        <h3 class="panel-title"><?php echo $stage->getTitle() . ' Stage'; ?></h3>
         <?php echo __('Group access'); ?>
       </div>
       <div class="alert alert-success" id="alertdiv" name="alertdiv" style="display: none;">
-        <button type="button" class="close" onClick="document.getElementById('alertdiv').style.display = 'none';" aria-hidden="true">&times;</button>
+        <button type="button" class="close" onClick="document.getElementById('alertdiv').style.display = 'none';"
+          aria-hidden="true">&times;</button>
         <strong><?php echo __('Well done'); ?>!</strong> <?php echo __('You successfully updated this stage'); ?></a>.
       </div>
       <div class="panel-body padding-0">
         <div class="form-group">
-        <label class="col-sm-4"><i class="bold-label"><?php echo __('Group access'); ?></i></label>
-        <div class="col-sm-8">
-          <select name='allowed_groups[]' id='allowed_groups' multiple>
-            <?php
+          <label class="col-sm-4"><i class="bold-label"><?php echo __('Group access'); ?></i></label>
+          <div class="col-sm-8">
+            <select name='allowed_groups[]' id='allowed_groups' multiple>
+              <?php
               $selected = "";
               $q = Doctrine_Query::create()
-                 ->from("MfGuardGroup a")
-                 ->orderBy("a.name ASC");
+                ->from("MfGuardGroup a")
+                ->orderBy("a.name ASC");
               $groups = $q->execute();
-              foreach($groups as $group)
-              {
+              foreach ($groups as $group) {
                 $selected = "";
                 $grouppermissions = $group->getPermissions();
-                foreach($grouppermissions as $grouppermission)
-                {
-                    $q = Doctrine_Query::create()
-                       ->from("MfGuardPermission a")
-                       ->where("a.name = ?", "accesssubmenu".$stage->getId());
-                    $permission = $q->fetchOne();
-                    if($permission) {
-                      if ($permission->getId() == $grouppermission->getId()) {
-                        $selected = "selected";
-                      }
+                foreach ($grouppermissions as $grouppermission) {
+                  $q = Doctrine_Query::create()
+                    ->from("MfGuardPermission a")
+                    ->where("a.name = ?", "accesssubmenu" . $stage->getId());
+                  $permission = $q->fetchOne();
+                  if ($permission) {
+                    if ($permission->getId() == $grouppermission->getId()) {
+                      $selected = "selected";
                     }
+                  }
                 }
-                echo "<option value='".$group->getId()."' ".$selected.">".$group->getName()."</option>";
+                echo "<option value='" . $group->getId() . "' " . $selected . ">" . $group->getName() . "</option>";
               }
-            ?>
-          </select>
-        </div>
-        </div>
-        <div class="form-group">
-        <label class="col-sm-4"><i class="bold-label"><?php echo __('Add a new group'); ?></i></label>
-        <div class="col-sm-8">
-          <div id="newgroup" name="newgroup">
+              ?>
+            </select>
           </div>
-          <div class="col-sm-12" style="margin-top: 10px;" align="right">
-          <button type="button" class="btn btn-primary" id="addnewgroup" name="addnewgroup"><?php echo __('+ Add Group'); ?></button>
-          <br><br>
+        </div>
+        <?php if ($sf_user->mfHasCredential('access_security')) {
+          ?>
+          <div class="form-group">
+            <label class="col-sm-4"><i class="bold-label"><?php echo __('Add a new group'); ?></i></label>
+            <div class="col-sm-8">
+              <div id="newgroup" name="newgroup">
+              </div>
+              <div class="col-sm-12" style="margin-top: 10px;" align="right">
+                <button type="button" class="btn btn-primary" id="addnewgroup"
+                  name="addnewgroup"><?php echo __('+ Add Group'); ?></button>
+                <br><br>
+              </div>
+              <script>
+                jQuery(document).ready(function () {
+
+                  $("#addnewgroup").click(function () {
+                    $("#newgroup").append("<div class='form-group' class='formgroup'><label class='col-sm-4'><?php echo __('Group'); ?></label><div class='col-sm-8'><input type='text' name='name[]' class='form-control' placeholder='<?php echo __('Group Name'); ?>'></div><a style='float: right; margin-top: 10px;' href='#' class='panel-close' onClick='$(this).closest(\"div\").remove();'>&times;</a></div>");
+                  });
+
+                });
+              </script>
+            </div>
           </div>
-          <script>
-            jQuery(document).ready(function(){
-
-              $( "#addnewgroup" ).click(function() {
-                  $("#newgroup").append("<div class='form-group' class='formgroup'><label class='col-sm-4'><?php echo __('Group'); ?></label><div class='col-sm-8'><input type='text' name='name[]' class='form-control' placeholder='<?php echo __('Group Name'); ?>'></div><a style='float: right; margin-top: 10px;' href='#' class='panel-close' onClick='$(this).closest(\"div\").remove();'>&times;</a></div>");
-              });
-
-            });
-            </script>
-        </div>
-        </div>
+        <?php } ?>
       </div>
-      <div class="panel-footer">
-           <a class="btn btn-danger mr10" href="/plan/stages/index/filter/<?php echo $stage->getMenuId(); ?>"><?php echo __('Back to Workflow'); ?></a><button type="submit" class="btn btn-primary" name="submitbuttonname" id="submitbuttonname" value="submitbuttonvalue"><?php echo __('Submit'); ?></button>
-     </div>
-   </div>
 
-    </form>
+      <div class="panel-footer">
+        <a class="btn btn-danger mr10"
+          href="/plan/stages/index/filter/<?php echo $stage->getMenuId(); ?>"><?php echo __('Back to Workflow'); ?></a>
+        <?php if ($sf_user->mfHasCredential('access_security')) {
+          ?>
+          <button type="submit" class="btn btn-primary" name="submitbuttonname" id="submitbuttonname"
+            value="submitbuttonvalue"><?php echo __('Submit'); ?></button>
+        <?php } ?>
+      </div>
+
+    </div>
+
+  </form>
 </div>
 
 <script type="text/javascript" src="/assets_backend/js/jquery.bootstrap-duallistbox.js"></script>
 <script>
-jQuery(document).ready(function(){
-  var groups = $('[id="allowed_groups"]').bootstrapDualListbox();
-});
+  jQuery(document).ready(function () {
+    var groups = $('[id="allowed_groups"]').bootstrapDualListbox();
+  });
 </script>
--- webapp/flowengine/apps/backend/modules/stages/templates/indexSuccess.php
+++ webapp/flowengine/apps/backend/modules/stages/templates/indexSuccess.php
@@ -1,74 +1,89 @@
 <?php
 use_helper("I18N");
 
-if($sf_user->mfHasCredential("managestages"))
-{
-?>
-<div class="contentpanel">
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h3 class="panel-title"><?php if($service){ echo $service->getTitle(); ?>  <?php } ?></h3>
-			<?php if($service){ echo $service->getTitle(); ?>  <?php } ?><?php echo __('workflow'); ?> <?php echo __('stages'); ?>
-		</div>
-
-		<div class="panel-heading text-right">
-				<a class="btn btn-primary" id="newstage" href="/plan/stages/new/filter/<?php echo $filter; ?>"><?php echo __('+ Add New Stage'); ?></a>
-		</div>
-
-		<div class="panel-body p-b-0">
-			<?php
-			$list_of_stages = array();
-
-			foreach ($stages as $stage)
-			{
-				$list_of_stages[$stage->getOrderNo()] = $stage->getTitle();
-			}
-
-			$count = 0;
-			foreach ($stages as $stage)
-			{
-				$count++;
-				?>
-				<div class="list-group">
-				<div class="list-group-item">
-								<?php echo $stage->getTitle(); ?>
-				</div>
-				<div class="list-group-item">
-						<a class="btn btn-primary btn-sm btn-form" style="margin-right: 10px;" href="/plan/stages/actions/id/<?php echo $stage->getId(); ?>"><span class="fa fa-edit"></span> Actions</a>
-						<a class="btn btn-primary btn-sm btn-form" style="margin-right: 10px;" href="/plan/stages/groups/id/<?php echo $stage->getId(); ?>"><span class="fa fa-edit"></span> Group Access</a>
-						<a class="btn btn-primary btn-sm btn-form" style="margin-right: 10px;" href="/plan/stages/tasks/id/<?php echo $stage->getId(); ?>"><span class="fa fa-edit"></span> Allowed Tasks</a>
-						<a class="btn btn-primary btn-sm btn-form" style="margin-right: 10px;" href="/plan/stages/inspections/id/<?php echo $stage->getId(); ?>"><span class="fa fa-edit"></span> Inspections</a>
-
-
-						<div class="btn-group pull-right m-l-10" role="group" aria-label="...">
-						<a class="btn btn-default btn-sm" href="/plan/stages/edit/id/<?php echo $stage->getId(); ?>/filter/<?php echo $filter; ?>"><span class="fa fa-edit"></span></a>
-						<a class="btn btn-default btn-sm" href="/plan/stages/delete/id/<?php echo $stage->getId(); ?>" onClick="if(confirm('Are you sure?')){ return true; }else{ return false; }"><span class="fa fa fa-trash-o"></span></a>
-						</div>
-
-						<select class='form-control pull-right' style="width: 200px;" onChange="window.location='/plan/stages/index/move/<?php echo $stage->getId(); ?>/to/' + this.value;">
-							<option>Change order...</option>
-							<option value="1">- Top -</option>
-							<?php
-							foreach($list_of_stages as $key => $value)
-							{
-									echo "<optgroup label='".$value."'>";
-										echo "<option value='".($key+1)."'>Here</option>";
-									echo "</optgroup>";
-							}
-							?>
-						</select>
-				</div>
+if ($sf_user->mfHasCredential("managestages")) {
+	?>
+	<div class="contentpanel">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title"><?php if ($service) {
+					echo $service->getTitle(); ?> 	<?php } ?></h3>
+				<?php if ($service) {
+					echo $service->getTitle(); ?> 	<?php } ?> 	<?php echo __('workflow'); ?>
+				<?php echo __('stages'); ?>
 			</div>
-		<?php
-		}
-		?>
-	</div>
-	</div><!--panel-body-->
-</div><!--panel-default-->
-<?php
-}
-else
-{
-  include_partial("accessdenied");
+
+			<div class="panel-heading text-right">
+				<a class="btn btn-primary" id="newstage"
+					href="/plan/stages/new/filter/<?php echo $filter; ?>"><?php echo __('+ Add New Stage'); ?></a>
+			</div>
+
+			<div class="panel-body p-b-0">
+				<?php
+				$list_of_stages = array();
+
+				foreach ($stages as $stage) {
+					$list_of_stages[$stage->getOrderNo()] = $stage->getTitle();
+				}
+
+				$count = 0;
+				foreach ($stages as $stage) {
+					$count++;
+					?>
+					<div class="list-group">
+						<div class="list-group-item">
+							<?php echo $stage->getTitle(); ?>
+						</div>
+						<div class="list-group-item">
+							
+							<a class="btn btn-primary btn-sm btn-form" style="margin-right: 10px;"
+								href="/plan/stages/actions/id/<?php echo $stage->getId(); ?>"><span class="fa fa-edit"></span>
+								Actions</a>
+							<a class="btn btn-primary btn-sm btn-form" style="margin-right: 10px;"
+								href="/plan/stages/groups/id/<?php echo $stage->getId(); ?>"><span class="fa fa-edit"></span>
+								Group Access</a>
+							<a class="btn btn-primary btn-sm btn-form" style="margin-right: 10px;"
+								href="/plan/stages/tasks/id/<?php echo $stage->getId(); ?>"><span class="fa fa-edit"></span>
+								Allowed Tasks</a>
+							<a class="btn btn-primary btn-sm btn-form" style="margin-right: 10px;"
+								href="/plan/stages/inspections/id/<?php echo $stage->getId(); ?>"><span
+									class="fa fa-edit"></span> Inspections</a>
+
+							<?php if ($sf_user->mfHasCredential('access_security')) {
+								?>
+								<div class="btn-group pull-right m-l-10" role="group" aria-label="...">
+
+									<a class="btn btn-default btn-sm"
+										href="/plan/stages/edit/id/<?php echo $stage->getId(); ?>/filter/<?php echo $filter; ?>"><span
+											class="fa fa-edit"></span></a>
+									<a class="btn btn-default btn-sm" href="/plan/stages/delete/id/<?php echo $stage->getId(); ?>"
+										onClick="if(confirm('Are you sure?')){ return true; }else{ return false; }"><span
+											class="fa fa fa-trash-o"></span></a>
+								</div>
+
+								<select class='form-control pull-right' style="width: 200px;"
+									onChange="window.location='/plan/stages/index/move/<?php echo $stage->getId(); ?>/to/' + this.value;">
+									<option>Change order...</option>
+									<option value="1">- Top -</option>
+									<?php
+									foreach ($list_of_stages as $key => $value) {
+										echo "<optgroup label='" . $value . "'>";
+										echo "<option value='" . ($key + 1) . "'>Here</option>";
+										echo "</optgroup>";
+									}
+									?>
+								</select>
+							<?php } ?>
+						</div>
+					</div>
+					<?php
+				}
+				?>
+			</div>
+		</div><!--panel-body-->
+	</div><!--panel-default-->
+	<?php
+} else {
+	include_partial("accessdenied");
 }
 ?>

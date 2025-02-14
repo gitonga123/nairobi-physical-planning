19d98227b2b84afc1738f61d555720cc6f103c54
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2025-02-14 12:53:21 +0300

    Send smss we are testing what is happening

--- webapp/flowengine/apps/backend/modules/stages/templates/indexSuccess.php
+++ webapp/flowengine/apps/backend/modules/stages/templates/indexSuccess.php
@@ -1,49 +1,44 @@
 <?php
 use_helper("I18N");
 
-if($sf_user->mfHasCredential("managestages"))
-{
-?>
-<div class="contentpanel">
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h3 class="panel-title"><?php if($service){ echo $service->getTitle(); ?>  <?php } ?></h3>
-			<?php if($service){ echo $service->getTitle(); ?>  <?php } ?><?php echo __('workflow'); ?> <?php echo __('stages'); ?>
-		</div>
+if ($sf_user->mfHasCredential("managestages")) {
+	?>
+	<div class="contentpanel">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title"><?php if ($service) {
+					echo $service->getTitle(); ?> 	<?php } ?></h3>
+				<?php if ($service) {
+					echo $service->getTitle(); ?> 	<?php } ?> 	<?php echo __('workflow'); ?>
+				<?php echo __('stages'); ?>
+			</div>
 
-		<div class="panel-heading text-right">
-				<a class="btn btn-primary" id="newstage" href="/backend.php/stages/new/filter/<?php echo $filter; ?>"><?php echo __('+ Add New Stage'); ?></a>
-		</div>
-
-		<div class="panel-body p-b-0">
 			<?php
-			$list_of_stages = array();
-
-			foreach ($stages as $stage)
-			{
-				$list_of_stages[$stage->getOrderNo()] = $stage->getTitle();
-			}
-
-			$count = 0;
-			foreach ($stages as $stage)
-			{
-				$count++;
+			if ($sf_user->mfHasCredential('access_security')) {
 				?>
 				<div class="panel-heading text-right">
 					<a class="btn btn-primary" id="newstage"
 						href="/backend.php/stages/new/filter/<?php echo $filter; ?>"><?php echo __('+ Add New Stage'); ?></a>
 				</div>
-				<div class="list-group-item">
-						<a class="btn btn-primary btn-sm btn-form" style="margin-right: 10px;" href="/backend.php/stages/actions/id/<?php echo $stage->getId(); ?>"><span class="fa fa-edit"></span> Actions</a>
-						<a class="btn btn-primary btn-sm btn-form" style="margin-right: 10px;" href="/backend.php/stages/groups/id/<?php echo $stage->getId(); ?>"><span class="fa fa-edit"></span> Group Access</a>
-						<a class="btn btn-primary btn-sm btn-form" style="margin-right: 10px;" href="/backend.php/stages/tasks/id/<?php echo $stage->getId(); ?>"><span class="fa fa-edit"></span> Allowed Tasks</a>
-						<a class="btn btn-primary btn-sm btn-form" style="margin-right: 10px;" href="/backend.php/stages/inspections/id/<?php echo $stage->getId(); ?>"><span class="fa fa-edit"></span> Inspections</a>
+			<?php } ?>
 
+			<div class="panel-body p-b-0">
+				<?php
+				$list_of_stages = array();
 
-						<div class="btn-group pull-right m-l-10" role="group" aria-label="...">
-						<a class="btn btn-default btn-sm" href="/backend.php/stages/edit/id/<?php echo $stage->getId(); ?>/filter/<?php echo $filter; ?>"><span class="fa fa-edit"></span></a>
-						<a class="btn btn-default btn-sm" href="/backend.php/stages/delete/id/<?php echo $stage->getId(); ?>" onClick="if(confirm('Are you sure?')){ return true; }else{ return false; }"><span class="fa fa fa-trash-o"></span></a>
+				foreach ($stages as $stage) {
+					$list_of_stages[$stage->getOrderNo()] = $stage->getTitle();
+				}
+
+				$count = 0;
+				foreach ($stages as $stage) {
+					$count++;
+					?>
+					<div class="list-group">
+						<div class="list-group-item">
+							<?php echo $stage->getTitle(); ?>
 						</div>
+						<div class="list-group-item">
 
 							<a class="btn btn-primary btn-sm btn-form" style="margin-right: 10px;"
 								href="/backend.php/stages/actions/id/<?php echo $stage->getId(); ?>"><span class="fa fa-edit"></span>
@@ -89,16 +84,10 @@ if($sf_user->mfHasCredential("managestages"))
 				}
 				?>
 			</div>
-		<?php
-		}
-		?>
-	</div>
-	</div><!--panel-body-->
-</div><!--panel-default-->
-<?php
-}
-else
-{
-  include_partial("accessdenied");
+		</div><!--panel-body-->
+	</div><!--panel-default-->
+	<?php
+} else {
+	include_partial("accessdenied");
 }
 ?>

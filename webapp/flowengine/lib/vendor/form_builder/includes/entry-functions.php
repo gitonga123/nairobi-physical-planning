cc33ce604df83ca748639fb67cdca97ff8137593
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2025-04-05 18:51:15 +0300

    Remove logs: rookie mistake on the settings, I had set only to show on the client side, its now sorted

--- webapp/flowengine/lib/vendor/form_builder/includes/entry-functions.php
+++ webapp/flowengine/lib/vendor/form_builder/includes/entry-functions.php
@@ -520,10 +520,7 @@ function mf_get_entry_details($dbh, $form_id, $entry_id, $options = array())
 		$form_elements[$i]['element_matrix_allow_multiselect'] = $row['matrix_multiselect_status'];
 		$form_elements[$i]['element_existing_stage'] = $row['element_existing_stage'];
 		$form_elements[$i]['element_existing_form'] = $row['element_existing_form'];
-
-		error_log("1. Element mark file with qr code ---> form id {$form_id}----> element_id {$row['element_id']} ----> element mark file with qr code {$row['element_mark_file_with_qr_code']}");
-		error_log(json_encode($row));
-
+		
 		//store element title into array for reference later
 		$element_title_lookup[$row['element_id']] = $row['element_title'];
 		$form_elements[$i]['element_mark_file_with_qr_code'] = $row['element_mark_file_with_qr_code'];//OTB Addition QR on attachments
@@ -693,10 +690,6 @@ function mf_get_entry_details($dbh, $form_id, $entry_id, $options = array())
 		$entry_details[$i]['element_option_query'] = $element_option_query;
 		$entry_details[$i]['element_existing_form'] = $element_existing_form;
 		$entry_details[$i]['element_existing_stage'] = $element_existing_stage;
-
-		error_log("2. Element mark file with qr code ---> form id {$form_id}----> element_id {$element['element_id']} ----> element mark file with qr code {$element['element_mark_file_with_qr_code']}");
-		error_log(json_encode($element));
-
 
 		if ('simple_name' == $element_type) { //Simple Name - 2 elements
 			$simple_name_value = trim($entry_data['element_' . $element_id . '_1'] . ' ' . $entry_data['element_' . $element_id . '_2']);
@@ -898,8 +891,6 @@ function mf_get_entry_details($dbh, $form_id, $entry_id, $options = array())
 					//encode the long query string for more readibility
 					//OTB Africa Start Add QR on attachments
 					//Check if we should mark with QR code according to field properties
-					error_log("3. Element mark file with qr code ---> form id {$form_id}----> element_id {$element_id} ----> element mark file with qr code {$element_mark_file_with_qr_code} ----> element fire qr all pages ---> {$element_file_qr_all_pages} ---> element file qr page position { $element_file_qr_page_position}");
-					error_log(json_encode($element));
 					if ($element_mark_file_with_qr_code == 1 and $element_file_qr_users == "clients" and empty(sfContext::getInstance()->getUser()->getAttribute('userid'))) {
 						$q_string = base64_encode("form_id={$form_id}&id={$entry_id}&el=element_{$element_id}&element_mark_file_with_qr_code={$element_mark_file_with_qr_code}&element_file_qr_all_pages={$element_file_qr_all_pages}&element_file_qr_page_position={$element_file_qr_page_position}&hash={$filename_md5}");
 					} else if ($element_mark_file_with_qr_code == 1 and $element_file_qr_users == "reviewers" and !empty(sfContext::getInstance()->getUser()->getAttribute('userid'))) {

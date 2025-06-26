370328d8dedec5b6add639c0c87bdc33bfc046fb
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2025-06-26 10:58:37 +0300

    Update convert number to words function

--- webapp/flowengine/lib/OTBHelper.php
+++ webapp/flowengine/lib/OTBHelper.php
@@ -920,13 +920,90 @@ class OTBHelper
         return $monthName;
     }
 
+    public function convert_number_to_words($number)
+    {
+        $hyphen = '-';
+        $conjunction = ' and ';
+        $separator = ', ';
+        $negative = 'negative ';
+        $decimal = ' point ';
+        $dictionary = [
+            0 => 'zero',
+            1 => 'one',
+            2 => 'two',
+            3 => 'three',
+            4 => 'four',
+            5 => 'five',
+            6 => 'six',
+            7 => 'seven',
+            8 => 'eight',
+            9 => 'nine',
+            10 => 'ten',
+            11 => 'eleven',
+            12 => 'twelve',
+            13 => 'thirteen',
+            14 => 'fourteen',
+            15 => 'fifteen',
+            16 => 'sixteen',
+            17 => 'seventeen',
+            18 => 'eighteen',
+            19 => 'nineteen',
+            20 => 'twenty',
+            30 => 'thirty',
+            40 => 'forty',
+            50 => 'fifty',
+            60 => 'sixty',
+            70 => 'seventy',
+            80 => 'eighty',
+            90 => 'ninety',
+            100 => 'hundred',
+            1000 => 'thousand',
+            1000000 => 'million',
+            1000000000 => 'billion',
+            1000000000000 => 'trillion',
+            1000000000000000 => 'quadrillion',
+            1000000000000000000 => 'quintillion'
+        ];
+
+        if (!is_numeric($number)) {
+            return false;
+        }
+
+        if ((int) $number < 0) {
+            return $negative . $this->convert_number_to_words(abs($number));
+        }
+
+        if ($number < 21) {
+            return $dictionary[$number];
+        } elseif ($number < 100) {
+            $tens = ((int) ($number / 10)) * 10;
+            $units = $number % 10;
+            return $dictionary[$tens] . ($units ? $hyphen . $dictionary[$units] : '');
+        } elseif ($number < 1000) {
+            $hundreds = (int) ($number / 100);
+            $remainder = $number % 100;
+            return $dictionary[$hundreds] . ' hundred' . ($remainder ? $conjunction . $this->convert_number_to_words($remainder) : '');
+        } else {
+            $baseUnit = pow(1000, floor(log($number, 1000)));
+            $numBaseUnits = (int) ($number / $baseUnit);
+            $remainder = $number % $baseUnit;
+            $string = $this->convert_number_to_words($numBaseUnits) . ' ' . $dictionary[$baseUnit];
+            if ($remainder) {
+                $string .= $remainder < 100 ? $conjunction : $separator;
+                $string .= $this->convert_number_to_words($remainder);
+            }
+            return $this->uppercaseString($string);
+        }
+    }
+
+
     /**
      *  Kibes Idea
      * @param $number
      * @return boolean
      * 
      */
-    public function convert_number_to_words($number)
+    public function convert_number_to_words_2($number)
     {
 
         $hyphen = '-';

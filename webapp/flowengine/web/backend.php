2bb26892ee995edf5ab764f81579b8581ade0f96
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2025-07-22 16:25:53 +0300

    Cors: Updated backend.php to fully support CORS (including preflight)

--- webapp/flowengine/web/backend.php
+++ webapp/flowengine/web/backend.php
@@ -1,5 +1,15 @@
 <?php
 
+// CORS Headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Handle preflight OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 require_once(__DIR__.'/../config/ProjectConfiguration.class.php');
 

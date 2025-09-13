6bf35f52a8ce2a2e283f2a4b75d2f90cf78e7d85
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2025-09-13 16:09:48 +0300

    fix (permission): hide application history to only admins

--- webapp/flowengine/apps/backend/modules/applications/templates/viewSuccess.php
+++ webapp/flowengine/apps/backend/modules/applications/templates/viewSuccess.php
@@ -325,7 +325,7 @@ use_helper("I18N");
 									<li <?php if ($current_tab == "memo") {
 											echo "class='active'";
 										} ?>><a href="/plan/applications/view?id=<?php echo $application->getId(); ?>&current_tab=memo"><span class="fa fa-comments-o"></span> <?php echo __('Memo  '); ?><span class="badge"><?php echo $internal_memo; ?></span></a></li>
-									<li <?php if ($current_tab == "history" && $sf_user->mfHasCredential("access_application_history")) {
+									<li <?php if ($sf_user->mfHasCredential("access_application_history") && $current_tab == "history") {
 											echo "class='active'";
 										} ?>><a href="/plan/applications/view?id=<?php echo $application->getId(); ?>&current_tab=history"><span class="fa fa-bars"></span> <?php echo __('Application History'); ?></a></li>
 								</ul>

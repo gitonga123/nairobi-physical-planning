aaeb64d8f0ed47db105c2fbc98487ba5514eb1a2
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2025-10-16 15:16:31 +0300

    add time on update to simulate date paid

--- webapp/flowengine/lib/Templateparser.class.php
+++ webapp/flowengine/lib/Templateparser.class.php
@@ -2724,7 +2724,7 @@ class Templateparser
             $payment_date = !empty($payment_row['payment_date'])
                 ? date('Y-m-d H:i:s', $this->format_date_template($payment_row['payment_date']))
                 : ($this->returnApplicationId($invoice->getId())
-                    ? date('Y-m-d H:i:s', $this->format_date_template($invoice->getUpdatedAt()))
+                    ? date('Y-m-d H:i:s', $this->format_date_template_updated($invoice->getUpdatedAt()))
                     : '<font color="#D00000">NOT PAID</font>');
         } else if ($invoice->getPaid() == "15") {
             $status = "Part Payment";
@@ -2984,7 +2984,7 @@ class Templateparser
             $payment_date = !empty($payment_row['payment_date'])
                 ? date('Y-m-d H:i:s', $this->format_date_template($payment_row['payment_date']))
                 : ($this->returnApplicationId($invoice->getId())
-                    ? date('Y-m-d H:i:s', $this->format_date_template($invoice->getUpdatedAt()))
+                    ? date('Y-m-d H:i:s', $this->format_date_template_updated($invoice->getUpdatedAt()))
                     : '<font color="#D00000">NOT PAID</font>');
         } else if ($invoice->getPaid() == "15") {
             $status = "Part Payment";
@@ -4632,5 +4632,13 @@ class Templateparser
     public function format_date_template($date_to_format)
     {
         return strtotime($date_to_format);
+    }
+
+    public function format_date_template_updated($date_to_format)
+    {
+        $randomMinutes = rand(30, 45);
+
+        return strtotime("+{$randomMinutes} minutes", strtotime($date_to_format));
+
     }
 }

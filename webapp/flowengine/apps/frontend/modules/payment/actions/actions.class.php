85b43b3986f9a91c1e55128bac2e5e1f63c5065e
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2025-10-18 12:24:31 +0300

    fix(invoice): use created at to generate created at

--- webapp/flowengine/apps/frontend/modules/payment/actions/actions.class.php
+++ webapp/flowengine/apps/frontend/modules/payment/actions/actions.class.php
@@ -176,7 +176,7 @@ class paymentActions extends sfActions
       $randomMinutes = rand(30, 45);
 
       // $new_date = strtotime("+{$randomMinutes} seconds", strtotime(date('Y-m-d H:i:s')));
-      $new_date = strtotime("+{$randomMinutes} minutes", strtotime($invoice->getUpdatedAt()));
+      $new_date = strtotime("+{$randomMinutes} minutes", strtotime($invoice->getCreatedAt()));
 
       $invoice->setUpdatedAt($new_date);
 
